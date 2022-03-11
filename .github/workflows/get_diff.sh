324ff9e825b42ca8492dd8b757633c92aa69eee1
ikeda yukari <y-ikeda@nextninja.co.jp>
2022-03-11 19:52:00 +0900

    change yml file

--- .github/workflows/get_diff.sh
+++ .github/workflows/get_diff.sh
@@ -2,7 +2,7 @@
 
 upload_files=($(sed -nr 's|^abcde: (.+) to aa://.+$|\1|p' ./.github/workflows/log.txt))
 echo "upload_files : ${upload_files[@]}"
-combine=(${upload_files[@]} ${1})
+combine=(${upload_files[@]} "$@")
 echo "combine : ${combine[@]}"
 diff=$((printf '%s\n' ${combine[@]} | sort -u; printf '%s\n' ${upload_files[@]}) | sort | uniq -u)
 echo "diff : ${diff[@]}"
