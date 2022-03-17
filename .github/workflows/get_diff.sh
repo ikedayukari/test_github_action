f45a6803d792cded8d02b3b822a8c3a8f77c52c2
ikeda yukari <y-ikeda@nextninja.co.jp>
2022-03-17 12:43:02 +0900

    [skip ci]

--- .github/workflows/get_diff.sh
+++ .github/workflows/get_diff.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 upload_files=($(sed -nr 's|^abcde: (.+) to aa://.+$|\1|p' ./.github/workflows/log.txt))
-echo "upload_files : ${upload_files[@]}"
-combine=(${upload_files[@]} "$@")
-echo "combine : ${combine[@]}"
-diff=$((printf '%s\n' ${combine[@]} | sort -u; printf '%s\n' ${upload_files[@]}) | sort | uniq -u)
-echo "diff : ${diff[@]}"
+echo "upload_files : " "${upload_files[@]}"
+combine=("${upload_files[@]}" "$@")
+echo "combine : " "${combine[@]}"
+diff=($( (printf '%s\n' "${combine[@]}" | sort -u; printf '%s\n' "${upload_files[@]}") | sort | uniq -u))
+echo "diff : " "${diff[@]}"
+
 for file in "${diff[@]}"; do
     echo "${file}"
 done
