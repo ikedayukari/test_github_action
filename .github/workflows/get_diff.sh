99a116ef54c46d57400cfd7984ad70f5e94175da
ikeda yukari <y-ikeda@nextninja.co.jp>
2022-03-22 18:16:57 +0900

    display environment variables in sh file

--- .github/workflows/get_diff.sh
+++ .github/workflows/get_diff.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-diff_files=$(echo "${1}" | tr ' ' '\n')
-
-upload_files=$(sed -nr 's|^abcde: (.+) to aa://.+$|\1|p' ./.github/workflows/log.txt)
-echo "upload_files :" "${upload_files}"
-
-combine="${upload_files}
-${diff_files}"
-echo "combine :" "${combine}"
-
-diff=($( (echo "${combine}" | sort -u; echo "${upload_files}") | sort | uniq -u))
-echo "diff :" "${diff[@]}"
-
-for file in "${diff[@]}"; do
-    echo "${file}"
-done
+echo "${ github.ref_name }"
