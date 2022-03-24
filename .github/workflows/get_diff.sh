4fde61026c3bf1a0efa88fae20fbc713a40ab7b8
ikeda yukari <y-ikeda@nextninja.co.jp>
2022-03-24 13:36:54 +0900

    file separately

--- .github/workflows/get_diff.sh
+++ .github/workflows/get_diff.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -s test.txt ]; then
+  curl -s -X POST "https://api.chatwork.com/v2/rooms/${ROOM_ID}/files" \
+    -H "X-ChatWorkToken: ${ABCDEF}" \
+    -F "file=@test.txt" \
+    -F "message="@- <<EOS
+  【 ブランチ：${BRANCH} 】
+  ${MESSAGE}(${AUTHOR})
+EOS
+else
+  curl -s -X POST "https://api.chatwork.com/v2/rooms/${ROOM_ID}/messages" \
+    -H "X-ChatWorkToken: ${ABCDEF}" \
+    -d "body="@- <<EOS
+  ${MESSAGE}(${AUTHOR})
+EOS
+fi
