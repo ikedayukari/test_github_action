8f8208f313274c58041caf9b4d46a3efe0cadc69
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-01-10 15:26:58 +0900

    $VAR $(uname) `uname` \$ \` \" \\

--- .github/workflows/get_diff.sh
+++ .github/workflows/get_diff.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-if [ -e test.txt ]; then
-  curl -s -X POST "https://api.chatwork.com/v2/rooms/${ROOM_ID}/files" \
-    -H "X-ChatWorkToken: ${ABCDEF}" \
-    -F "file=@test.txt" \
-    -F "message="@- <<EOS
-【 ブランチ：${BRANCH} 】
-${MESSAGE}(${AUTHOR})
-EOS
-else
-  curl -s -X POST "https://api.chatwork.com/v2/rooms/${ROOM_ID}/messages" \
-    -H "X-ChatWorkToken: ${ABCDEF}" \
-    -d @- <<EOS
-body=${MESSAGE}(${AUTHOR})
-EOS
-fi
+echo "aa"
